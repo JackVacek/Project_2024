a9d4aff1e1aaa7ace368a4c7369c90cf5a5cd5de
Christopher Vu <christophervu03@gmail.com>
2024-10-16 11:58:15 -0500

    Added Caliper regions and Adiak

--- MPI_Builds/bitonicsort.cpp
+++ MPI_Builds/bitonicsort.cpp
@@ -1,162 +1,209 @@
-  #include <iostream>
-  #include <string>
-  #include <cmath>
-  #include <random>
-  #include <algorithm>
-  #include <vector>
+#include <iostream>
+#include <string>
+#include <cmath>
+#include <random>
+#include <algorithm>
 
-  #include "mpi.h"
+#include "mpi.h"
+#include <caliper/cali.h>
+#include <caliper/cali-manager.h>
+#include <adiak.hpp>
 
-  using namespace std;
+using namespace std;
 
-  #define MASTER 0
+#define MASTER 0
 
-  void printArray(int* arr, int n) {
-      for (int i = 0; i < n; ++i) {
-          cout << arr[i] << " ";
-      }
-      cout << endl;
-  }
+void printArray(int* arr, int n) {
+    for (int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
 
-  int main(int argc, char** argv) {
-      // Initializing MPI
-      MPI_Init(&argc, &argv);
+int main(int argc, char** argv) {
+    CALI_CXX_MARK_FUNCTION;
 
-      // Get number of processes and current process rank
-      int num_processes, rank, n;
-      MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
-      MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-      n = stoi(argv[1]);
-      string input_type = argv[2];
-      // Create a new local array based on the number of processors
-      int localSize = n / num_processes;
-      int* localArray = new int[localSize];
+    // Initializing MPI
+    MPI_Init(&argc, &argv);
 
-      if (input_type == "Random") {
-          for (int i = 0; i < localSize; i++) {
-              localArray[i] = rand() % n;
-          }
-      } else if (input_type == "Sorted") {
-          for (int i = 0; i < localSize; i++) {
-              localArray[i] = i + rank * localSize;
-          }
-      } else if (input_type == "ReverseSorted") {
-          for (int i = 0; i < localSize; i++) {
-              localArray[i] = n - i - 1 - rank * localSize;
-          }
-      } else if (input_type == "1_perc_perturbed") {
-          for (int i = 0; i < localSize; i++) {
-              localArray[i] = i + rank * localSize;
-          }
+    // Get number of processes and current process rank
+    int num_processes, rank, n;
+    MPI_Comm_size(MPI_COMM_WORLD, &num_processes);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    n = stoi(argv[1]);
+    string input_type = argv[2];
 
-          int numPerturbed = localSize / 100;
+    // Create a new local array based on the number of processors
+    int localSize = n / num_processes;
+    int* localArray = new int[localSize];
 
-          for (int i = 0; i < numPerturbed; ++i) {
-              int idx1 = rand() % localSize;
-              int idx2 = rand() % localSize;
-              std::swap(localArray[idx1], localArray[idx2]);
-          }
-      }
-      // printing out rank and local array
-      // cout << "Rank: " << rank << " Local Array: ";
-      // printArray(localArray, localSize);
-      
-      // Distribute the input array to all processes
-      // MPI_Scatter(input_array, localSize, MPI_INT, localArray, localSize, MPI_INT, MASTER, MPI_COMM_WORLD);
+    cali::ConfigManager mgr;
+    mgr.start();
 
-      sort(localArray, localArray + localSize);
+    CALI_MARK_BEGIN("data_init_runtime");
+    if (input_type == "Random") {
+        for (int i = 0; i < localSize; i++) {
+            localArray[i] = rand() % n;
+        }
+    } else if (input_type == "Sorted") {
+        for (int i = 0; i < localSize; i++) {
+            localArray[i] = i + rank * localSize;
+        }
+    } else if (input_type == "ReverseSorted") {
+        for (int i = 0; i < localSize; i++) {
+            localArray[i] = n - i - 1 - rank * localSize;
+        }
+    } else if (input_type == "1_perc_perturbed") {
+        for (int i = 0; i < localSize; i++) {
+            localArray[i] = i + rank * localSize;
+        }
 
-      int num_stages = log2(num_processes);
+        int numPerturbed = localSize / 100;
 
-      for (int stage = 1; stage <= num_stages; stage++) {
-          for (int step = stage; step > 0; step--) {
-              int partner_rank = rank ^ (1 << (step - 1));
+        for (int i = 0; i < numPerturbed; ++i) {
+            int idx1 = rand() % localSize;
+            int idx2 = rand() % localSize;
+            std::swap(localArray[idx1], localArray[idx2]);
+        }
+    }
+    CALI_MARK_END("data_init_runtime");
 
-              // Calculate the group number and the direction of the sort
-              int group_size = 1 << stage;
-              int group = rank / group_size;
-              bool ascending = (group % 2 == 0);
+    CALI_MARK_BEGIN("comp");
+    CALI_MARK_BEGIN("comp_large");
+    sort(localArray, localArray + localSize);
+    CALI_MARK_END("comp_large");
+    CALI_MARK_END("comp");
 
-              int* partner_arr = new int[localSize];
-              MPI_Sendrecv(localArray, localSize, MPI_INT, partner_rank, 0, partner_arr, localSize, MPI_INT, partner_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    int num_stages = log2(num_processes);
 
-              // Merge the local array with the partner array (while maintaining sorted order)
-              int* merged = new int[localSize * 2];
-              int i = 0, j = 0, k = 0;
+    for (int stage = 1; stage <= num_stages; stage++) {
+        for (int step = stage; step > 0; step--) {
 
-              while (i < localSize && j < localSize) {
-                  if (localArray[i] <= partner_arr[j]) {
-                      merged[k++] = localArray[i++];
-                  } else {
-                      merged[k++] = partner_arr[j++];
-                  }
-              }
+            CALI_MARK_BEGIN("comp");
+            CALI_MARK_BEGIN("comp_small");
+            int partner_rank = rank ^ (1 << (step - 1));
 
-              while (i < localSize) {
-                  merged[k++] = localArray[i++];
-              }
+            // Calculate the group number and the direction of the sort
+            int group_size = 1 << stage;
+            int group = rank / group_size;
+            bool ascending = (group % 2 == 0);
 
-              while (j < localSize) {
-                  merged[k++] = partner_arr[j++];
-              }
-              
-              // Copy the merged array back to the local array based on the direction of the sort
-              if (rank < partner_rank && ascending || rank > partner_rank && !ascending) {
-                  for (int i = 0; i < localSize; i++) {
-                      localArray[i] = merged[i];
-                  }
-              } else {
-                  for (int i = 0; i < localSize; i++) {
-                      localArray[i] = merged[i + localSize];
-                  }
-              }
+            int* partner_arr = new int[localSize];
+            CALI_MARK_END("comp_small");
+            CALI_MARK_END("comp");
 
-              delete[] partner_arr;
-              delete[] merged;
-          }
-          MPI_Barrier(MPI_COMM_WORLD);
-      }
+            CALI_MARK_BEGIN("comm");
+            CALI_MARK_BEGIN("comm_large");
+            MPI_Sendrecv(localArray, localSize, MPI_INT, partner_rank, 0, partner_arr, localSize, MPI_INT, partner_rank, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            CALI_MARK_END("comm_large");
+            CALI_MARK_END("comm");
 
-      // MPI_Gather(localArray, localSize, MPI_INT, input_array, localSize, MPI_INT, MASTER, MPI_COMM_WORLD);
+            CALI_MARK_BEGIN("comp");
+            CALI_MARK_BEGIN("comp_large");
+            // Merge the local array with the partner array (while maintaining sorted order)
+            int* merged = new int[localSize * 2];
+            int i = 0, j = 0, k = 0;
 
-      // Checking if our local array sorted correctly
-      bool localSorted = true;
-      for (int i = 1; i < localSize; ++i) {
-          if (localArray[i] < localArray[i-1]) {
-              localSorted = false;
-              break;
-          }
-      }
+            while (i < localSize && j < localSize) {
+                if (localArray[i] <= partner_arr[j]) {
+                    merged[k++] = localArray[i++];
+                } else {
+                    merged[k++] = partner_arr[j++];
+                }
+            }
 
-      // Checking to see that the minimum element in the next rank is >= the local maximum element
-      int localMax = localArray[localSize - 1];
-      int nextMin = 0;
-      bool boundaryCheck = true;
+            while (i < localSize) {
+                merged[k++] = localArray[i++];
+            }
 
-      if (rank < num_processes - 1) {
-          MPI_Send(&localMax, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
-      }
+            while (j < localSize) {
+                merged[k++] = partner_arr[j++];
+            }
+            
+            // Copy the merged array back to the local array based on the direction of the sort
+            if (rank < partner_rank && ascending || rank > partner_rank && !ascending) {
+                for (int i = 0; i < localSize; i++) {
+                    localArray[i] = merged[i];
+                }
+            } else {
+                for (int i = 0; i < localSize; i++) {
+                    localArray[i] = merged[i + localSize];
+                }
+            }
 
-      if (rank != 0) {
-          MPI_Recv(&nextMin, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-          boundaryCheck = (nextMin <= localArray[0]);
-      }
+            delete[] partner_arr;
+            delete[] merged;
+            CALI_MARK_END("comp_large");
+            CALI_MARK_END("comp");
+        }
+        CALI_MARK_BEGIN("comm");
+        MPI_Barrier(MPI_COMM_WORLD);
+        CALI_MARK_END("comm");
+    }
 
-      bool isSorted = localSorted && boundaryCheck;
+    CALI_MARK_BEGIN("correctness_check");
+    // Correctness check
+    // Checking if our local array sorted correctly
+    bool localSorted = true;
+    for (int i = 1; i < localSize; ++i) {
+        if (localArray[i] < localArray[i-1]) {
+            localSorted = false;
+            break;
+        }
+    }
 
-      int globalSorted;
-      MPI_Allreduce(&isSorted, &globalSorted, 1, MPI_INT, MPI_LAND, MPI_COMM_WORLD);
+    // Checking to see that the minimum element in the next rank is >= the local maximum element
+    int localMax = localArray[localSize - 1];
+    int nextMin = 0;
+    bool boundaryCheck = true;
 
-      if (rank == MASTER) {
-          if (globalSorted) {
-              cout << "Bitonic sort PASSED" << endl;
-          } else {
-              cout << "Bitonic sort FAILED" << endl;
-          }
-      }
+    if (rank < num_processes - 1) {
+        MPI_Send(&localMax, 1, MPI_INT, rank + 1, 0, MPI_COMM_WORLD);
+    }
 
-      delete[] localArray;
+    if (rank != 0) {
+        MPI_Recv(&nextMin, 1, MPI_INT, rank - 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        boundaryCheck = (nextMin <= localArray[0]);
+    }
 
-      MPI_Finalize();
-      return 0;
-  }
+    bool isSorted = localSorted && boundaryCheck;
+
+    int globalSorted;
+    MPI_Allreduce(&isSorted, &globalSorted, 1, MPI_INT, MPI_LAND, MPI_COMM_WORLD);
+    CALI_MARK_END("correctness_check");
+
+    if (rank == MASTER) {
+        if (globalSorted) {
+            cout << "Bitonic sort PASSED" << endl;
+        } else {
+            cout << "Bitonic sort FAILED" << endl;
+        }
+    }
+
+    delete[] localArray;
+
+    if (rank == MASTER) {
+        adiak::init(NULL);
+        adiak::launchdate();    // launch date of the job
+        adiak::libraries();     // Libraries used
+        adiak::cmdline();       // Command line used to launch the job
+        adiak::clustername();   // Name of the cluster
+        adiak::value("algorithm", "bitonic"); // The name of the algorithm you are using (e.g., "merge", "bitonic")
+        adiak::value("programming_model", "mpi"); // e.g. "mpi"
+        adiak::value("data_type", "int"); // The datatype of input elements (e.g., double, int, float)
+        adiak::value("size_of_data_type", sizeof(int)); // sizeof(datatype) of input elements in bytes (e.g., 1, 2, 4)
+        adiak::value("input_size", n); // The number of elements in input dataset (1000)
+        adiak::value("input_type", input_type); // For sorting, this would be choices: ("Sorted", "ReverseSorted", "Random", "1_perc_perturbed")
+        adiak::value("num_procs", num_processes); // The number of processors (MPI ranks)
+        adiak::value("scalability", "strong"); // The scalability of your algorithm. choices: ("strong", "weak")
+        adiak::value("group_num", "1"); // The number of your group (integer, e.g., 1, 10)
+        adiak::value("implementation_source", "handwritten"); // Where you got the source code of your algorithm. choices: ("online", "ai", "handwritten").
+    }
+
+    // Flush Caliper output before finalizing MPI
+    mgr.stop();
+    mgr.flush();
+
+    MPI_Finalize();
+    return 0;
+}

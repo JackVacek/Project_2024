87175954b7f8dcbcc64f3d9abe11b2aaf85f9d5e
Zak Borman <72626364+zakborman@users.noreply.github.com>
2024-10-22 17:24:14 -0500

    Refactored radixsort and parallelized data_init_runtime

--- radixsort/radixsort.cpp
+++ radixsort/radixsort.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <random>
+#include <ctime>
 
 #include <mpi.h>
 #include <caliper/cali.h>
@@ -9,21 +11,107 @@
 
 using namespace std;
 
-// Local counting sort for one digit
-void counting_sort(vector<int>& arr, int digit) {
-    vector<int> sorted(arr.size());
-    int count[10] = {0};
+vector<int> random(size_t size, size_t total_size, int rank) {
+    mt19937 gen(time(0) + rank * 1000);
+    uniform_int_distribution<> dist(0, total_size);
+    vector<int> vec(size);
+    generate(vec.begin(), vec.end(), [&]() { return dist(gen); });
+    return vec;
+}
 
-    for (int i = 0; i < arr.size(); ++i)
-        count[(arr[i] / digit) % 10]++;
-    
-    for (int i = 1; i < 10; ++i)
-        count[i] += count[i-1];
+vector<int> sorted(size_t size, size_t total_size, int rank) {
+    vector<int> vec;
+    for (size_t i = rank * size; i < size * (rank + 1); i++) {
+        vec.push_back(i);
+    }
+    return vec;
+}
 
-    for (int i = arr.size() - 1; i >= 0; --i)
-        sorted[--count[(arr[i] / digit) % 10]] = arr[i];
+vector<int> reversed(size_t size, size_t total_size, int rank) {
+    vector<int> vec;
+    for (size_t i = total_size - (size * rank); i > total_size - size * (rank + 1); i--) {
+        vec.push_back(i);
+    }
+    return vec;
+}
+
+vector<int> perturbed(size_t size, size_t total_size, int rank) {
+    vector<int> vec = sorted(size, total_size, rank);
+    mt19937 gen(time(0) + rank * 1000);
+    uniform_int_distribution<> dist(0, total_size);
+    uniform_int_distribution<> perturb(0, 99);
+    for (size_t i = 0; i < size; i++) {
+        if (perturb(gen) == 0) {
+            vec[i] = dist(gen);
+        }
+    }
+    return vec;
+}
+
+vector<int> data_init_runtime(int rank, int num_procs, int input_size, string input_type, int local_size) {
+    CALI_CXX_MARK_FUNCTION;
     
-    arr = sorted;
+    vector<int> data;
+
+    if (input_type == "Random") {
+        data = random(local_size, input_size, rank);
+    } else if (input_type == "Sorted") {
+        data = sorted(local_size, input_size, rank);
+    } else if (input_type == "ReverseSorted") {
+        data = reversed(local_size, input_size, rank);
+    } else if (input_type == "1_perc_perturbed") {
+        data = perturbed(local_size, input_size, rank);
+    }
+
+    return data;
+}
+
+void radix_sort(vector<int>& arr) {
+    int max = *max_element(arr.begin(), arr.end());
+
+    for (int digit = 1; max / digit > 0; digit *= 10) {
+        vector<int> sorted(arr.size());
+        int count[10] = {0};
+
+        for (int i = 0; i < arr.size(); ++i)
+            count[(arr[i] / digit) % 10]++;
+        
+        for (int i = 1; i < 10; ++i)
+            count[i] += count[i-1];
+
+        for (int i = arr.size() - 1; i >= 0; --i)
+            sorted[--count[(arr[i] / digit) % 10]] = arr[i];
+        
+        arr = sorted;
+    }   
+}
+
+vector<int> merge_sorted(vector<int> arr, int num_procs, int local_size) {
+    vector<vector<int>> parts(num_procs);
+    for (int i = 0; i < num_procs; ++i)
+        parts[i] = vector<int>(arr.begin() + i * local_size, arr.begin() + (i+1) * local_size);
+
+    vector<int> sorted = parts[0];
+    for (int i = 1; i < parts.size(); ++i) {
+        vector<int> merged(sorted.size() + parts[i].size());
+        merge(sorted.begin(), sorted.end(), parts[i].begin(), parts[i].end(), merged.begin());
+        sorted = merged;
+    }
+
+    return sorted;
+}
+
+void correctness_check(vector<int> arr) {
+    CALI_CXX_MARK_FUNCTION;
+    
+    for (int i = 1; i < arr.size(); ++i) {
+            if (arr[i] < arr[i-1]) {
+                cout << "Incorrect" << endl;
+                return;
+            }
+            //cout << arr[i] << " ";
+        }
+    cout << "Correct" << endl;
 }
 
 int main(int argc, char* argv[]) {
@@ -40,7 +128,7 @@ int main(int argc, char* argv[]) {
 
     if (argc < 3) {
         if (rank == 0) {
-            std::cerr << "Usage: " << argv[0]
+            cerr << "Usage: " << argv[0]
                       << " <input_size> <input_type>\n"
                       << "input_size: 2^16, 2^18, 2^20, ..., 2^28\n"
                       << "input_type: Sorted, ReverseSorted, Random, 1_perc_perturbed\n";
@@ -51,47 +139,14 @@ int main(int argc, char* argv[]) {
     
     int input_size = atoi(argv[1]);
     string input_type = argv[2];
-    
-    CALI_MARK_BEGIN("data_init_runtime");
-    vector<int> arr(input_size);
-    if (rank == 0) {
-        srand(time(NULL));
-        for (int i = 0; i < input_size; ++i) {
-            if (input_type == "Sorted" || input_type == "1_perc_perturbed")
-                arr[i] = i;
-            else if (input_type == "ReverseSorted")
-                arr[i] = input_size - i - 1;
-            else if (input_type == "Random")
-                arr[i] = rand() % input_size;
-        }
-        if (input_type == "1_perc_perturbed") {
-            for (int i = 0; i < input_size / 100; ++i)
-                swap(arr[rand() % input_size], arr[rand() % input_size]);
-        }
-    }
-    CALI_MARK_END("data_init_runtime");
-
-    CALI_MARK_BEGIN("comm");
-    CALI_MARK_BEGIN("comm_large");
-    MPI_Bcast(&input_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    CALI_MARK_END("comm_large");
-    CALI_MARK_END("comm");
 
     int local_size = input_size / num_procs;
-    vector<int> local(local_size);
-
-    CALI_MARK_BEGIN("comm");
-    CALI_MARK_BEGIN("comm_large");
-    MPI_Scatter(arr.data(), local_size, MPI_INT, local.data(), local_size, MPI_INT, 0, MPI_COMM_WORLD);
-    CALI_MARK_END("comm_large");
-    CALI_MARK_END("comm");
+    vector<int> local = data_init_runtime(rank, num_procs, input_size, input_type, local_size);
 
     // Radix sort
     CALI_MARK_BEGIN("comp");
     CALI_MARK_BEGIN("comp_large");
-    int max = *max_element(local.begin(), local.end());
-    for (int digit = 1; max / digit > 0; digit *= 10)
-        counting_sort(local, digit);
+    radix_sort(local);
     CALI_MARK_END("comp_large");
     CALI_MARK_END("comp");
 
@@ -104,31 +159,13 @@ int main(int argc, char* argv[]) {
     CALI_MARK_END("comm");
 
     if (rank == 0) {
-        // Merge sorted local arrays
-        vector<vector<int>> parts(num_procs);
-        for (int i = 0; i < num_procs; ++i)
-            parts[i] = vector<int>(global.begin() + i * local_size, global.begin() + (i+1) * local_size);
-
         CALI_MARK_BEGIN("comp");
         CALI_MARK_BEGIN("comp_large");
-        vector<int> sorted = parts[0];
-        for (int i = 1; i < parts.size(); ++i) {
-            vector<int> merged(sorted.size() + parts[i].size());
-            merge(sorted.begin(), sorted.end(), parts[i].begin(), parts[i].end(), merged.begin());
-            sorted = merged;
-        }
+        vector<int> sorted = merge_sorted(global, num_procs, local_size);
         CALI_MARK_END("comp_large");
         CALI_MARK_END("comp");
         
-        CALI_MARK_BEGIN("correctness_check");
-        for (int i = 1; i < sorted.size(); ++i) {
-            if (sorted[i] < sorted[i-1]) {
-                cout << "Incorrect" << endl;
-                break;
-            }
-        }
-        cout << "Correct" << endl;
-        CALI_MARK_END("correctness_check");
+        correctness_check(sorted);
     }
 
     adiak::init(NULL);
@@ -143,7 +180,7 @@ int main(int argc, char* argv[]) {
     adiak::value("input_size", input_size); // The number of elements in input dataset (1000)
     adiak::value("input_type", input_type); // For sorting, this would be choices: ("Sorted", "ReverseSorted", "Random", "1_perc_perturbed")
     adiak::value("num_procs", num_procs); // The number of processors (MPI ranks)
-    adiak::value("scalability", "strong"); // The scalability of your algorithm. choices: ("strong", "weak")
+    adiak::value("scalability", "weak"); // The scalability of your algorithm. choices: ("strong", "weak")
     adiak::value("group_num", 1); // The number of your group (integer, e.g., 1, 10)
     adiak::value("implementation_source", "handwritten"); // Where you got the source code of your algorithm. choices: ("online", "ai", "handwritten").
 

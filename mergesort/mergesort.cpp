b5b8c83bede496e894ceaa94b9d1f0d40b81f563
Jack Vacek <jackvacek21@tamu.edu>
2024-10-28 14:48:50 -0500

    Updated merge sort algorithm

--- mergesort/mergesort.cpp
+++ mergesort/mergesort.cpp
@@ -16,15 +16,14 @@
 #define FROM_MASTER 1 /* setting a message type */
 #define FROM_WORKER 2 /* setting a message type */
 
-void choose_array_type(int *a, std::string type, int sizeOfArray)
+void choose_array_type(int *a, std::string type, int sizeOfArray, int offset, int totalSize)
 {
+    std::mt19937 gen(static_cast<unsigned int>(std::time(0) + offset));
+    std::uniform_int_distribution<> dist(1, totalSize);
     // std::cout << "The array type is: " << type << std::endl;
     if (type == "Random")
     {
         // Random
-        std::mt19937 gen(static_cast<unsigned int>(std::time(0)));
-        std::uniform_int_distribution<> dist(1, sizeOfArray);
-
         for (int i = 0; i < sizeOfArray; ++i)
         {
             a[i] = dist(gen);
@@ -34,19 +33,16 @@ void choose_array_type(int *a, std::string type, int sizeOfArray)
     {
         // std::cout << "is reverse sorted" << std::endl;
         //  reverse sorted
-        int index = 0;
-        for (int i = sizeOfArray - 1; i >= 0; --i)
+        int start = totalSize - offset;
+        for (int i = 0; i < sizeOfArray; ++i)
         {
-            a[index] = i + 1;
-            index += 1;
+            a[i] = start - i;
         }
     }
 
     if (type == "1_perc_perturbed")
     {
         // 1% perturbed
-        std::mt19937 gen(static_cast<unsigned int>(std::time(0)));
-        std::uniform_int_distribution<> dist(1, sizeOfArray);
         std::uniform_int_distribution<> perturb(0, 99);
 
         for (int i = 0; i < sizeOfArray; ++i)
@@ -57,7 +53,7 @@ void choose_array_type(int *a, std::string type, int sizeOfArray)
             }
             else
             {
-                a[i] = i + 1;
+                a[i] = offset + i + 1;
             }
         }
     }
@@ -66,7 +62,7 @@ void choose_array_type(int *a, std::string type, int sizeOfArray)
         // sorted
         for (int i = 0; i < sizeOfArray; i++)
         {
-            a[i] = i + 1;
+            a[i] = offset + i + 1;
         }
     }
 
@@ -183,6 +179,7 @@ int main(int argc, char *argv[])
         rc;       /* return code */
 
     /* Define Caliper region names */
+
     const char *data_init_runtime = "data_init_runtime";
     const char *correctness_check = "correctness_check";
     const char *comm = "comm";
@@ -196,7 +193,6 @@ int main(int argc, char *argv[])
     MPI_Comm_rank(MPI_COMM_WORLD, &taskid);   // get process id
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks); // get number of processes
 
-    int *a = new int[sizeOfArray];
     int localArraySize = sizeOfArray / numtasks;
     int *localArrays = new int[localArraySize];
     // std::cout << localArraySize << std::endl;
@@ -210,48 +206,94 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    if (taskid == 0)
+    int offset = taskid * localArraySize;
+    CALI_MARK_BEGIN(data_init_runtime);
+    choose_array_type(localArrays, typeOfArray, localArraySize, offset, sizeOfArray);
+    CALI_MARK_END(data_init_runtime);
+    /*
+    printf("Task %d received the following array segment: ", taskid);
+    for (int i = 0; i < localArraySize; i++)
     {
-        CALI_MARK_BEGIN(data_init_runtime);
-        choose_array_type(a, typeOfArray, sizeOfArray);
-        CALI_MARK_END(data_init_runtime);
-        // for (int i = 0; i < sizeOfArray; ++i)
-        //{
-        //    std::cout << a[i] << " ";
-        //}
-        // std::cout << std::endl;
+        printf("%d ", localArrays[i]);
     }
-    CALI_MARK_BEGIN(comm);
-    CALI_MARK_BEGIN(comm_large);
-    MPI_Scatter(a, localArraySize, MPI_INT, localArrays, localArraySize, MPI_INT, MASTER, MPI_COMM_WORLD);
-    CALI_MARK_END(comm_large);
-    CALI_MARK_END(comm);
-
+    printf("\n");
+    */
     CALI_MARK_BEGIN(comp);
-    CALI_MARK_BEGIN(comp_small);
+    CALI_MARK_BEGIN(comp_large);
     mergeSort(localArrays, 0, localArraySize - 1);
-    CALI_MARK_END(comp_small);
+    CALI_MARK_END(comp_large);
     CALI_MARK_END(comp);
 
-    CALI_MARK_BEGIN(comm);
-    CALI_MARK_BEGIN(comm_large);
-    MPI_Gather(localArrays, localArraySize, MPI_INT, a, localArraySize, MPI_INT, MASTER, MPI_COMM_WORLD);
-    CALI_MARK_END(comm_large);
-    CALI_MARK_END(comm);
-
-    if (taskid == 0)
+    // CALI_MARK_BEGIN(comm);
+    // CALI_MARK_BEGIN(comm_large);
+    // MPI_Gather(localArrays, localArraySize, MPI_INT, a, localArraySize, MPI_INT, MASTER, MPI_COMM_WORLD);
+    // CALI_MARK_END(comm_large);
+    // CALI_MARK_END(comm);
+    int step = 1;
+    while (step < numtasks)
     {
-        CALI_MARK_BEGIN(comp);
-        CALI_MARK_BEGIN(comp_large);
-        for (int i = 1; i < numtasks; i++)
+        if (taskid % (2 * step) == 0)
         {
-            merge(a, 0, (i * localArraySize) - 1, (i + 1) * localArraySize - 1);
+            if (taskid + step < numtasks)
+            {
+                int recvSize;
+                CALI_MARK_BEGIN(comm);
+                CALI_MARK_BEGIN(comm_small);
+                MPI_Recv(&recvSize, 1, MPI_INT, taskid + step, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                CALI_MARK_END(comm_small);
+                CALI_MARK_END(comm);
+                int *recvArray = new int[recvSize];
+                CALI_MARK_BEGIN(comm);
+                CALI_MARK_BEGIN(comm_large);
+                MPI_Recv(recvArray, recvSize, MPI_INT, taskid + step, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+                CALI_MARK_END(comm_large);
+                CALI_MARK_END(comm);
+
+                CALI_MARK_BEGIN(comp);
+                CALI_MARK_BEGIN(comp_large);
+                int newSize = localArraySize + recvSize;
+                int *newArray = new int[newSize];
+
+                for (int i = 0; i < localArraySize; i++)
+                {
+                    newArray[i] = localArrays[i];
+                }
+                for (int i = 0; i < recvSize; i++)
+                {
+                    newArray[localArraySize + i] = recvArray[i];
+                }
+                merge(newArray, 0, localArraySize - 1, newSize - 1);
+                delete[] recvArray;
+                delete[] localArrays;
+                localArrays = newArray;
+                localArraySize = newSize;
+                CALI_MARK_END(comp_large);
+                CALI_MARK_END(comp);
+            }
         }
-        CALI_MARK_END(comp_large);
-        CALI_MARK_END(comp);
+        else
+        {
+            int target = taskid - step;
+            CALI_MARK_BEGIN(comm);
+            CALI_MARK_BEGIN(comm_small);
+            MPI_Send(&localArraySize, 1, MPI_INT, target, 0, MPI_COMM_WORLD);
+            CALI_MARK_END(comm_small);
+            CALI_MARK_END(comm);
+            CALI_MARK_BEGIN(comm);
+            CALI_MARK_BEGIN(comm_large);
+            MPI_Send(localArrays, localArraySize, MPI_INT, target, 0, MPI_COMM_WORLD);
+            CALI_MARK_END(comm_large);
+            CALI_MARK_END(comm);
+            break;
+        }
+        step *= 2;
+    }
+
+    if (taskid == MASTER)
+    {
         bool isCorrect = false;
         CALI_MARK_BEGIN(correctness_check);
-        isCorrect = correctness_checker(a, sizeOfArray);
+        isCorrect = correctness_checker(localArrays, sizeOfArray);
         CALI_MARK_END(correctness_check);
         if (isCorrect)
         {
@@ -261,33 +303,34 @@ int main(int argc, char *argv[])
         {
             printf("The program is incorrect\n");
         }
-        // for (int i = 0; i < sizeOfArray; ++i)
-        //{
-        //     std::cout << a[i] << " ";
-        // }
-        // std::cout << std::endl;
+        /*
+        for (int i = 0; i < localArraySize; i++)
+        {
+            printf("%d ", localArrays[i]);
+        }
+        printf("\n");
+        */
     }
 
     mgr.stop();
     mgr.flush();
 
     adiak::init(NULL);
-    adiak::launchdate();                                // launch date of the job
-    adiak::libraries();                                 // Libraries used
-    adiak::cmdline();                                   // Command line used to launch the job
-    adiak::clustername();                               // Name of the cluster
-    adiak::value("algorithm", "merge");                 // The name of the algorithm you are using (e.g., "merge", "bitonic")
-    adiak::value("programming_model", "mpi");           // e.g. "mpi"
-    adiak::value("data_type", "int");                   // The datatype of input elements (e.g., double, int, float)
-    adiak::value("size_of_data_type", sizeof(int));     // sizeof(datatype) of input elements in bytes (e.g., 1, 2, 4)
-    adiak::value("input_size", sizeOfArray);            // The number of elements in input dataset (1000)
-    adiak::value("input_type", typeOfArray);            // For sorting, this would be choices: ("Sorted", "ReverseSorted", "Random", "1_perc_perturbed")
-    adiak::value("num_procs", numtasks);                // The number of processors (MPI ranks)
-    adiak::value("scalability", "weak");                // The scalability of your algorithm. choices: ("strong", "weak")
-    adiak::value("group_num", 1);                       // The number of your group (integer, e.g., 1, 10)
-    adiak::value("implementation_source", "online/ai/handwritten"); // Where you got the source code of your algorithm. choices: ("online", "ai", "handwritten").
+    adiak::launchdate();                             // launch date of the job
+    adiak::libraries();                              // Libraries used
+    adiak::cmdline();                                // Command line used to launch the job
+    adiak::clustername();                            // Name of the cluster
+    adiak::value("algorithm", "merge");              // The name of the algorithm you are using (e.g., "merge", "bitonic")
+    adiak::value("programming_model", "mpi");        // e.g. "mpi"
+    adiak::value("data_type", "int");                // The datatype of input elements (e.g., double, int, float)
+    adiak::value("size_of_data_type", sizeof(int));  // sizeof(datatype) of input elements in bytes (e.g., 1, 2, 4)
+    adiak::value("input_size", sizeOfArray);         // The number of elements in input dataset (1000)
+    adiak::value("input_type", typeOfArray);         // For sorting, this would be choices: ("Sorted", "ReverseSorted", "Random", "1_perc_perturbed")
+    adiak::value("num_procs", numtasks);             // The number of processors (MPI ranks)
+    adiak::value("scalability", "weak");             // The scalability of your algorithm. choices: ("strong", "weak")
+    adiak::value("group_num", 1);                    // The number of your group (integer, e.g., 1, 10)
+    adiak::value("implementation_source", "online"); // Where you got the source code of your algorithm. choices: ("online", "ai", "handwritten").
 
-    delete[] a;
     delete[] localArrays;
     MPI_Finalize();
 }
